d68f88b47317a4075413021c970cb53e335015c0
Jakub Ryška <ryskajakub@seznam.cz>
2021-01-10 23:13:14 +0100

    add docker

---
+++ code/src/Index.js
@@ -0,0 +1,11 @@
+// @ts-check
+const express = require("express");
+
+const app = express();
+const PORT = 8000;
+
+app.get('/', (req, res) => res.send('Express + TypeScript Server\n'));
+app.get('/vue', (req, res) => res.send('Express + TypeScript Server\n'));
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
